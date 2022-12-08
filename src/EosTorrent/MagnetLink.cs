c4c7a0def0aadd30aa17a0c4b165918c1ccfa588
solo <s.omsarashvili@area.ge>
2022-12-08 22:07:03 +0400

    MagnetLink + tests

---
+++ src/EosTorrent/MagnetLink.cs
@@ -0,0 +1,43 @@
+using System.Text.RegularExpressions;
+
+namespace EosTorrent;
+
+public class MagnetLink
+    {
+        public string? DisplayName { get; }
+        public IList<string> Trackers { get; }
+        public string Hash { get; }
+
+        public MagnetLink(string magnetLink)
+        {
+            DisplayName = ParseDisplayName(magnetLink);
+            Trackers = ParseTrackers(magnetLink);
+            Hash = ParseHash(magnetLink);
+        }
+
+        private static string? ParseDisplayName(string magnetLink)
+        {
+            var displayNameMatch = Regex.Match(magnetLink, "dn=([^&]*)");
+            if (displayNameMatch.Success)
+            {
+                return Uri.UnescapeDataString(displayNameMatch.Groups[1].Value);
+            }
+            return null;
+        }
+
+        private static IList<string> ParseTrackers(string magnetLink)
+        {
+            var trackersMatch = Regex.Matches(magnetLink, "tr=([^&]*)");
+            return trackersMatch.Select(m => Uri.UnescapeDataString(m.Groups[1].Value)).ToList();
+        }
+
+        private static string ParseHash(string magnetLink)
+        {
+            var hashMatch = Regex.Match(magnetLink, "xt=urn:btih:([^&]*)");
+            if (hashMatch.Success)
+            {
+                return hashMatch.Groups[1].Value;
+            }
+            return null;
+        }
+    }
---
+++ tests/UnitTests/MagnetLinkShould.cs
@@ -0,0 +1,32 @@
+using EosTorrent;
+using NUnit.Framework;
+
+namespace UnitTests;
+
+public class MagnetLinkShould
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void HaveHash()
+    {
+        var magnet = new MagnetLink("magnet:?xt=urn:btih:e3811b9539cacff680e418124272177c47477157");
+        Assert.That(magnet.Hash, Is.Not.Empty);
+        Assert.That(magnet.DisplayName, Is.Null);
+        Assert.That(magnet.Trackers, Is.Empty);
+    }
+
+    [Test]
+    public void HaveAllData()
+    {
+        var magnet = new MagnetLink(
+            "magnet:?xt=urn:btih:5b0b15ca5e5f793c7f9a9dccc7a4ab4bcfc7d605&dn=my-file.txt&tr=udp%3A%2F%2Ftracker.example.org%3A8080&tr=udp%3A%2F%2Ftracker.example.com%3A8080&xl=1024&as=http%3A%2F%2Fdownload.example.org%2Fmy-file.txt");
+        Assert.That(magnet.Hash, Is.EqualTo("5b0b15ca5e5f793c7f9a9dccc7a4ab4bcfc7d605"));
+        Assert.That(magnet.DisplayName, Is.EqualTo("my-file.txt"));
+        Assert.That(magnet.Trackers, Has.Exactly(1).Matches<string>(x => x == "udp://tracker.example.org:8080"));
+        Assert.That(magnet.Trackers, Has.Exactly(1).Matches<string>(x => x == "udp://tracker.example.com:8080"));
+    }
+}
